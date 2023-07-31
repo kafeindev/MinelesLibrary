633365b8e9095e95395b3c8f928079b1fd306bae
Osman Furkan <osmanfurkan115@gmail.com>
2023-07-31 17:17:25 +0300

    Initialize plugin template

---
+++ src/main/java/net/mineles/library/MinelesLibrary.java
@@ -0,0 +1,16 @@
+package net.mineles.library;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MinelesLibrary extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
