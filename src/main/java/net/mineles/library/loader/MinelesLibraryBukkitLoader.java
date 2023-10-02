323d2b7bbfc946e5d7cda584e17e870052e95d97
KafeinTR <kafein40@gmail.com>
2023-10-02 21:53:08 +0300

    Add bungee support and some refactors.

---
+++ src/main/java/net/mineles/library/loader/MinelesLibraryBukkitLoader.java
@@ -0,0 +1,6 @@
+package net.mineles.library.loader;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinelesLibraryBukkitLoader extends JavaPlugin {
+}
---
+++ src/main/java/net/mineles/library/loader/MinelesLibraryBungeeLoader.java
@@ -0,0 +1,6 @@
+package net.mineles.library.loader;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class MinelesLibraryBungeeLoader extends Plugin {
+}
