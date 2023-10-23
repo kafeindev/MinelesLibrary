913f3974e29fe048935463fb1e966e5a16465df9
Kafein <kafein40@gmail.com>
2023-10-23 23:49:17 +0300

    Write Teleport classes.

--- src/main/java/net/mineles/library/teleport/Teleport.java
+++ src/main/java/net/mineles/library/teleport/Teleport.java
@@ -1,4 +1,81 @@
 package net.mineles.library.teleport;
 
-public class Teleport {
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+public final class Teleport {
+    private final @NotNull UUID player;
+    private final @NotNull UUID target;
+    private final @NotNull TeleportType type;
+
+    public Teleport(@NotNull UUID player,
+                    @NotNull UUID target,
+                    @NotNull TeleportType type) {
+        this.player = player;
+        this.target = target;
+        this.type = type;
+    }
+
+    @NotNull
+    public static Teleport of(@NotNull UUID player,
+                              @NotNull UUID target,
+                              @NotNull TeleportType type) {
+        return new Teleport(player, target, type);
+    }
+
+    @NotNull
+    public static Teleport of(@NotNull String player,
+                              @NotNull String target,
+                              @NotNull String type) {
+        return new Teleport(UUID.fromString(player), UUID.fromString(target), TeleportType.valueOf(type));
+    }
+
+    @NotNull
+    public static Teleport request(@NotNull UUID player, @NotNull UUID target) {
+        return new Teleport(player, target, TeleportType.REQUEST);
+    }
+
+    @NotNull
+    public static Teleport accept(@NotNull UUID player, @NotNull UUID target) {
+        return new Teleport(player, target, TeleportType.ACCEPT);
+    }
+
+    @NotNull
+    public static Teleport deny(@NotNull UUID player, @NotNull UUID target) {
+        return new Teleport(player, target, TeleportType.DENY);
+    }
+
+    @NotNull
+    public static Teleport cancel(@NotNull UUID player, @NotNull UUID target) {
+        return new Teleport(player, target, TeleportType.CANCEL);
+    }
+
+    public @NotNull UUID getPlayer() {
+        return this.player;
+    }
+
+    public @NotNull UUID getTarget() {
+        return this.target;
+    }
+
+    public @NotNull TeleportType getType() {
+        return this.type;
+    }
+
+    @Override
+    public String toString() {
+        return "Teleport{" +
+                "player=" + this.player +
+                ", target=" + this.target +
+                ", type=" + this.type +
+                '}';
+    }
+
+    public enum TeleportType {
+        REQUEST,
+        ACCEPT,
+        DENY,
+        CANCEL
+    }
 }
--- src/main/java/net/mineles/library/teleport/TeleportDecoder.java
+++ src/main/java/net/mineles/library/teleport/TeleportDecoder.java
@@ -4,14 +4,17 @@ import com.google.gson.JsonObject;
 import net.mineles.library.redis.codec.Decoder;
 import org.jetbrains.annotations.NotNull;
 
+
 public final class TeleportDecoder implements Decoder<Teleport> {
     @Override
     public @NotNull Teleport decode(@NotNull JsonObject payload) {
-        return null;
+        return Teleport.of(payload.get("player").getAsString(),
+                payload.get("target").getAsString(),
+                payload.get("type").getAsString());
     }
 
     @Override
     public @NotNull String encode(@NotNull Teleport payload) {
-        return null;
+        return payload.toString();
     }
 }
