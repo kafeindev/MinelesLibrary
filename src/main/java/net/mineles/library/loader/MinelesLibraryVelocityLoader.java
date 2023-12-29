ee93d6fccd63c2ecfd66b1aa3a6441799ebb8272
kafein <kafein40@gmail.com>
2023-12-29 16:56:10 +0300

    Remove plugin annotation from MinelesLibraryVelocityLoader class, because we are use velocity-plugin.json instead.

--- src/main/java/net/mineles/library/loader/MinelesLibraryVelocityLoader.java
+++ src/main/java/net/mineles/library/loader/MinelesLibraryVelocityLoader.java
@@ -1,28 +1,13 @@
 package net.mineles.library.loader;
 
 import com.google.inject.Inject;
-import com.velocitypowered.api.event.Subscribe;
-import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
-import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
 
-@Plugin(
-        id = "MinelesLibrary",
-        name = "MinelesLibrary",
-        version = "1.0.0",
-        authors = "Mineles",
-        description = "Library for Mineles Network."
-)
 public final class MinelesLibraryVelocityLoader {
     private final ProxyServer server;
 
     @Inject
     public MinelesLibraryVelocityLoader(ProxyServer server) {
         this.server = server;
-    }
-
-    @Subscribe
-    public void onProxyInitialization(ProxyInitializeEvent event) {
-
     }
 }
