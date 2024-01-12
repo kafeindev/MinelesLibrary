67e33634d2ae4892ea8277acc8066caf55d30ed0
KafeinDev <kafein40@gmail.com>
2024-01-12 12:10:03 +0300

    Refactors for redis classes.

--- src/main/java/net/mineles/library/teleport/TeleportDecoder.java
+++ src/main/java/net/mineles/library/teleport/TeleportDecoder.java
@@ -1,20 +1,21 @@
 package net.mineles.library.teleport;
 
 import com.google.gson.JsonObject;
+import net.mineles.library.redis.RedisClient;
 import net.mineles.library.redis.codec.Decoder;
 import org.jetbrains.annotations.NotNull;
 
 
 public final class TeleportDecoder implements Decoder<Teleport> {
     @Override
-    public @NotNull Teleport decode(@NotNull JsonObject payload) {
+    public @NotNull Teleport decode(@NotNull JsonObject payload, @NotNull RedisClient redisClient) {
         return Teleport.of(payload.get("player").getAsString(),
                 payload.get("target").getAsString(),
                 payload.get("type").getAsString());
     }
 
     @Override
-    public @NotNull String encode(@NotNull Teleport payload) {
+    public @NotNull String encode(@NotNull Teleport payload, @NotNull RedisClient redisClient) {
         return payload.toString();
     }
 }
